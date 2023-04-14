6163ad1fe7d9fc0b5ae7e664bfa427acdf326d03
Rida Salman <ridasalman458@gmail.com>
2023-04-14 22:06:32 +0500

    Second Commit

--- lib/UI/welcome.dart
+++ lib/UI/welcome.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_10/UI/home.dart';
+import 'package:flutter_application_10/models/ciity.dart';
+import 'package:flutter_application_10/models/constants.dart';
 
 class Welcome extends StatefulWidget {
   const Welcome({super.key});
@@ -10,9 +13,81 @@ class Welcome extends StatefulWidget {
 class _WelcomeState extends State<Welcome> {
   @override
   Widget build(BuildContext context) {
+    List<City> cities =
+        City.citiesList.where((city) => city.isDefault == false).toList();
+    List<City> selectedCities = City.getSelectedCities();
+    Constants myConstants = Constants();
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: Text("Welcome"),
+        automaticallyImplyLeading: false,
+        backgroundColor: myConstants.secondaryColor,
+        title:
+            Center(child: Text(selectedCities.length.toString() + ' selected')),
+      ),
+      body: ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        itemCount: cities.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            margin: const EdgeInsets.only(left: 10, top: 20, right: 10),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            height: size.height * .08,
+            width: size.width,
+            decoration: BoxDecoration(
+                border: cities[index].isSelected == true
+                    ? Border.all(
+                        color: myConstants.secondaryColor.withOpacity(.6),
+                        width: 2,
+                      )
+                    : Border.all(color: Colors.white),
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+                boxShadow: [
+                  BoxShadow(
+                    color: myConstants.primaryColor.withOpacity(.2),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3),
+                  )
+                ]),
+            child: Row(
+              children: [
+                GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        cities[index].isSelected = !cities[index].isSelected;
+                      });
+                    },
+                    child: Image.asset(
+                      cities[index].isSelected == true
+                          ? 'assets/checked.png'
+                          : 'assets/unchecked.png',
+                      width: 30,
+                    )),
+                const SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  cities[index].city,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: cities[index].isSelected == true
+                        ? myConstants.primaryColor
+                        : Colors.black54,
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: myConstants.secondaryColor,
+        child: const Icon(Icons.pin_drop),
+        onPressed: () {
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => const Home()));
+        },
       ),
     );
   }
--- lib/models/constants.dart
+++ lib/models/constants.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 class Constants {
   final Color primaryColor = const Color(0xFF6c64fb);
-  final Color secondaryColor = const Color(0xff90B2F8);
+  final Color secondaryColor = const Color(0xff748cf1);
 }
